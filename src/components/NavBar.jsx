cd47df06a683e347429427d5106408dcba599842
MauroDavid512 <mauroalosactormza@gmail.com>
2023-05-30 14:51:00 -0300

    inicio portafolio

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+function NavBar() {
+    return (
+        < >
+            <div className="nav-bar">
+                <>Home</>
+                <>About</>
+                <>Experience</>
+                <>Skills</>
+                <>Portfolio</>
+                <>Contact</>
+            </div>
+        </>
+    );
+}
+
+export default NavBar;
